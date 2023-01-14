9fff574dfe02d06ad3ac3012bfac29aca543110e
leohsm <leonardo.siriaco@gmail.com>
2023-01-14 10:16:25 -0300

    major changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::io;
 
 fn main() {
-    let buffer = ler_string();
-    println!("{}", buffer);
+    let chosen = menu(vec!["A", "B", "C", "D", "E"]);
+    println!("Chosen: {}", chosen);
 }
 
-fn ler_string() -> String {
+fn read_string() -> String {
     let mut buffer = String::new();
 
     io::stdin()
@@ -13,4 +13,27 @@ fn ler_string() -> String {
         .expect("Failed to read line.");
 
     buffer.trim().to_string()
+}
+
+fn read_u8() -> u8 {
+    let buffer = loop {
+        match read_string().parse::<u8>() {
+            Ok(num) => break num,
+            Err(_) => continue,
+        };
+    };
+    buffer
+}
+
+fn menu(options: Vec<&str>) -> u8 {
+    let buffer = options.len();
+    for n in 0..buffer {
+        println!("{}) {}", n, options[n]);
+    }
+    loop {
+        let chosen = read_u8();
+        if chosen < buffer as u8 {
+            break chosen;
+        }
+    }
 }
